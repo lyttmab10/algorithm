c644e737f4eb17fa78cb79a1f87e873530c1e8c5
lyttmab10 <674259025@webmail.npru.ac.th>
2024-06-28 11:36:59 +0700

    first

---
+++ DisplayText.java
@@ -0,0 +1,9 @@
+public class DisplayText {
+    public static void main(String[] args) {
+        System.out.println("Saharat" + "Benjaisriluk ");
+        System.out.println(10 + 9);
+        System.out.println("NPRU " + 2567);
+        System.out.println(8 + "8" + 8);
+        System.out.println(26 + " 08 " + 2547);
+    }
+}
---
+++ Hellowold2.java
@@ -0,0 +1,5 @@
+public class Hellowold2 {
+    public static void main(String[] args) {
+    System.out.println("saharat!!!");
+}
+}
